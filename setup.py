06d05492e16c1ee3910f4fad9d76ee6784b7f4cf
Taeil Goh <taeil.goh@berkeley.edu>
2021-03-02 19:43:59 -0800

    initial structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Hierarchical Pretraining: Research Repository',
+    author='Berkeley-Data',
+    license='MIT',
+)
